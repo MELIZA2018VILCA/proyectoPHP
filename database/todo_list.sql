aa305bf64ac053d8e6b67d0453063edbdb4a2056
meliza <201913306k@utea.edu.pe>
2022-10-23 21:13:51 -0500

    documento md

--- database/todo_list.sql
+++ database/todo_list.sql
@@ -1,31 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 19, 2021 at 05:03 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `todo_list`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `todos`
---
+-- Database: `bd_todolist`
 
 CREATE TABLE `todos` (
   `id` int(11) NOT NULL,
@@ -35,10 +13,6 @@ CREATE TABLE `todos` (
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `todos`
---
-
 INSERT INTO `todos` (`id`, `title`, `description`, `date`, `user_id`) VALUES
 (1, 'Create a PHP Program', 'In this PHP program I will be create a Todo List with Multi User registration system. It means Multiple user can register their account and add todos.', '2021-09-15', 1),
 (2, 'Upload a YouTube Video', 'Just for testing.....', '2021-09-15', 1),
@@ -46,11 +20,7 @@ INSERT INTO `todos` (`id`, `title`, `description`, `date`, `user_id`) VALUES
 (4, 'Aliquam feugiat sapien in turpis dapibus, sit amet', 'Donec nec velit et velit scelerisque vehicula eget non sem.\r\nMauris ac purus nec nisi bibendum accumsan vel sit amet lectus.', '2021-09-15', 1),
 (5, 'Second user todo', 'here are some descriptions', '2021-09-15', 4);
 
--- --------------------------------------------------------
 
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -58,9 +28,6 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `users`
---
 
 INSERT INTO `users` (`id`, `email`, `password`) VALUES
 (1, 'purecodingofficial@gmail.com', '0139a3c5cf42394be982e766c93f5ed0'),
---
+++ practica1.md
@@ -0,0 +1,16 @@
+LISTADO DE  CLASES 
+    Se ha creado las tablas 
+•	Usuario
+•	Todos(tareas)
+En el gestor de base de datos mysql , la base de datos tiene el nombre de bd_todolist
+ MÉTODOS IMPLEMENTADOS
+Los métodos implementados son los siguientes
+    El de agregar usuarios
+    Añadir tareas tareas, dentro de esta se puede modificar o eliminar dichas tareas
+ 
+LISTADO DE PLANTILLAS AÑADIDAS.
+
+EXPLICACIÓN DE CÓDIGO FUENTE RELEVANTE DE LAS NUEVAS FUNCIONALIDADES IMPLEMENTADAS.
+ Dificultades
+ la conexion con la base de datos al usar como gestor de base de datos mySql 
+
